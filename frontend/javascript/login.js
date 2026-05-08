57d64f92a072e86df241e7b4cd8209e65b5c378b
ooaaiaeea <ottoekerman@gmail.com>
2026-05-08 12:31:51 +0200

    Skapat några html+css. Skrivit login script
    Har ej prövat ännu, finns säkert fel i koden

---
+++ frontend/javascript/login.js
@@ -0,0 +1,46 @@
+
+async function login(event){
+    event.preventDefault();
+
+    let email = document.querySelector("#email");
+    let password = document.querySelector("#password");
+
+    let loginData = {
+        email: email.value,
+        password: password.value
+    }
+
+    try {
+        loading.textContent = "Logging in. Loading..."
+        const request = new Request("/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(loginData),
+            credentials: "include"
+        })
+        const response = await fetch(request);
+
+        if (!response.ok){
+            console.log(`Login failed: ${response.status}`)
+            throw new Error()
+        }
+
+        loading.textcontent = "Success! Redirecting..."
+
+        //Ska man hamna i playlists här????????
+        window.location.href = "playlists.html";
+
+    } catch(error) {
+        //Hur ska jag göra här för att kunna avgöra vad som misslyckats? Serverns svar avgör om användaren
+        //skickat fel uppgifter eller om något gått fel?
+        loading.textContent = "Login failed."
+    }
+
+}
+
+let loginForm = document.querySelector("#login-form");
+let loading = document.querySelector("#loading");
+
+loginForm.addEventListener("submit", login);
