e48d8f243dc2c0d1ca1a39c9a211c27a91754ab4
ttSamtopia <s-linden@live.se>
2026-05-12 01:35:50 +0200

    Import all classes, created basic cookie handling

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
+// deno-lint-ignore no-unversioned-import
 import { serveFile, serveDir } from "jsr:@std/http/file-server";
+import { User } from "./javascript/users.js";
+import { Playlist } from "./javascript/playlists.js";
+import { Track } from "./javascript/tracks.js";
+import { Album } from "./javascript/albums.js";
+import { Artist } from "./javascript/artists.js";
+
 
 function handler(request) {
 	const options = {
@@ -10,15 +17,39 @@ function handler(request) {
 			"Access-Control-Allow-Methods": "GET,POST,PATCH,DELETE",
 		}
 	}
-	const url = new URL(request.url)
 	if (request.method == "OPTIONS") {
 		return new Response(null, options)
 	}
-	
-	// ! Add check for cookies and headers !
-	// Auto run no matter what?
-	// Always require authentication otherwise home page?
+	const url = new URL(request.url)
 
+
+	function createSessionId () {
+		const randomId = crypto.randomUUID();
+		options.headers["Set-Cookie"] = `sessionId=${randomId}, max-age=86400, httpOnly, sameSite=strict`;
+		return randomId;
+	}
+	function deleteSessionId () {
+		options.headers["Set-Cookie"] = `sessionId=; max-age=0, httpOnly, sameSite=strict`;
+	}
+	function checkAuth () {
+		const cookie = request.headers.get("cookie");
+		if (!cookie) {
+			return null
+		}
+		sessionId = cookie.split("=")[1]
+		const allUsers = User.getUsers();
+		for (const user of allUsers) {
+			if (user.sessionId == sessionId) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	
+	// if (url.pathname == "/api/auth/login" && request.method == "POST") {
+		
+	// }
 
 	if (url.pathname == "/themix" && request.method == "GET") {
 		return serveFile(request, "./../frontend/index.html")
